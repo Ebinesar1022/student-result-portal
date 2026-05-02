50ff1f4bb3cd5e831368301f0484ddeb2a1e40d5
Ebinesar <lourthu@cognultsoft.com>
2026-05-02 17:20:55 +0530

    added new fix

---
+++ src/api/asp-api/SchoolManagementAPI/Migrations/20260502113647_InitialCreate1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Classes_ClassId",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Photo",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClassId",
+                table: "Students",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Classes_ClassId",
+                table: "Students",
+                column: "ClassId",
+                principalTable: "Classes",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Classes_ClassId",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Photo",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClassId",
+                table: "Students",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Classes_ClassId",
+                table: "Students",
+                column: "ClassId",
+                principalTable: "Classes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
